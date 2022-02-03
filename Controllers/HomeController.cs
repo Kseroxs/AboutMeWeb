cf15ad7a79cc8f878d751a021d871163137575e2
Szymon <erbopl@gmail.com>
2022-02-03 16:07:02 +0100

    Dodaj pliki projektów.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using System.Xml.Linq;
+using Szymon.Models;
+
+namespace Szymon.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(string url = "http://www.rssmix.com/u/12834899/rss.xml")
+        {
+            var items = XElement.Load(url).Descendants("item").Select(i => new RssItem
+            {
+                Title = i.Element("title").Value,
+                PubDate = i.Element("pubDate").Value,
+                Description = i.Element("description").Value
+            }).ToList().Take(4);
+
+            return View(items);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
